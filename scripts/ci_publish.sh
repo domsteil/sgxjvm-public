2223ad58d1c579c2714ccdf55222578bacc76e8e
igor nitto <nitto.igor@gmail.com>
2018-12-17 18:33:26 +0000

    Moved k8s config under deployment folder; changed to publish with latest tag

--- scripts/ci_publish.sh
+++ scripts/ci_publish.sh
@@ -5,6 +5,6 @@ SCRIPT_DIR=$(dirname $(readlink -f $0))
 export REGISTRY_URL=$1
 export REGISTRY_USERNAME=$2
 export REGISTRY_PASSWORD=$3
-export DOCKER_VERSION_TAG=$(cd "$SCRIPT_DIR/.." && git describe --long --abbrev=10)
+export DOCKER_VERSION_TAG=latest
 
 cd "$SCRIPT_DIR/.." && ./gradlew :containers:publish -i
