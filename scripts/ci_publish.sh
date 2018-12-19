7f13298a9673d301aaa958b5ddee6c75af032952
igor nitto <nitto.igor@gmail.com>
2018-12-19 01:11:52 +0000

    ENT-2802: Preparing deployment and publishing scripts (#8)

---
+++ scripts/ci_publish.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+SCRIPT_DIR=$(dirname $(readlink -f $0))
+
+export REGISTRY_URL=$1
+export REGISTRY_USERNAME=$2
+export REGISTRY_PASSWORD=$3
+export DOCKER_VERSION_TAG=latest
+
+cd "$SCRIPT_DIR/.." && ./gradlew :containers:publish -i
