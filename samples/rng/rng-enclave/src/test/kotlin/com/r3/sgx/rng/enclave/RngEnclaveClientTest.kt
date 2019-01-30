68f877ca9e1c7db1674cbfdc9a3deff75c94e46e
Chris Rankin <chris.rankin@r3.com>
2019-01-30 10:58:25 +0000

    ENT-2799: Test simulated RNG enclave with its client. (#24)
    * Start new container before the tests, and stop it afterwards.
    * Add unit test for the RNG client against running enclave.
    * Publish Debug enclave and test the Simulation.
    * Ensure system property is set for unit tests.
    * Update environment variable names for Docker registry credentials.
    * Bind RNG client's shadowJar task to assemble instead of build.

---
+++ samples/rng/rng-enclave/src/test/kotlin/com/r3/sgx/rng/enclave/RngEnclaveClientTest.kt
@@ -0,0 +1,45 @@
+package com.r3.sgx.rng.enclave
+
+import com.r3.sgx.core.common.SchemesSettings
+import com.r3.sgx.enclavelethost.client.Crypto
+import com.r3.sgx.rng.client.RngEnclaveletHostClient
+import org.junit.BeforeClass
+import org.junit.Test
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.security.SecureRandom
+import java.util.*
+
+class RngEnclaveClientTest {
+    companion object {
+        private val logger: Logger = LoggerFactory.getLogger(RngEnclaveClientTest::class.java)
+        private val grpcPort: Int = getIntegerProperty("com.r3.sgx.enclave.simulated.grpc.port")
+
+        @BeforeClass
+        @JvmStatic
+        fun setup() {
+            logger.info("RNG Host: port={}", grpcPort)
+        }
+
+        private fun getIntegerProperty(name: String): Int
+            = Integer.getInteger(name) ?: fail("System property '$name' not set")
+
+        private fun fail(message: String): Nothing = throw AssertionError(message)
+    }
+
+    @Test
+    fun testHost() {
+        RngEnclaveletHostClient.withClient("localhost:$grpcPort") { client ->
+            val rngResponse = client.getRandomBytes()
+            val signatureSchemeFactory = Crypto.getSignatureSchemeFactory(SecureRandom.getInstance("SHA1PRNG"))
+            val eddsaScheme = signatureSchemeFactory.make(SchemesSettings.EDDSA_ED25519_SHA512)
+            eddsaScheme.verify(
+                eddsaScheme.decodePublicKey(rngResponse.publicKey),
+                rngResponse.signature,
+                rngResponse.randomBytes
+            )
+            val base64RandomBytes = Base64.getEncoder().encode(rngResponse.randomBytes)
+            logger.info("RandomBytes={}", String(base64RandomBytes))
+        }
+    }
+}
