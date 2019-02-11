53d91fea29ff83b6e4084e3294f1d30bc860c6d4
Andras Slemmer <andras.slemmer@r3cev.com>
2019-02-11 11:16:01 +0000

    Add release push as publish step

--- scripts/ci_publish.sh
+++ scripts/ci_publish.sh
@@ -2,4 +2,16 @@
 set -xeuo pipefail
 SCRIPT_DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
-cd "$SCRIPT_DIR/.." && ./gradlew :samples:rng:rng-enclave:pushEnclaveImageDebug -i
+cd "$SCRIPT_DIR/.."
+
+./gradlew :samples:rng:rng-enclave:buildEnclaveImageDebug -i
+if [[ ${OBLIVIUM_VERSIONING} = "release" ]]
+then
+  ./gradlew :samples:rng:rng-enclave:buildEnclaveImageRelease -i
+fi
+
+./gradlew :samples:rng:rng-enclave:pushEnclaveImageDebug -i
+if [[ ${OBLIVIUM_VERSIONING} = "release" ]]
+then
+  ./gradlew :samples:rng:rng-enclave:pushEnclaveImageRelease -i
+fi
